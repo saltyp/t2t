db60967642d63ce9c6d3f60ed426e82a98cec8c3
TrishDan <struligt@gmail.com>
2023-11-06 21:40:01 +0100

    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/App.js
+++ src/App.js
@@ -96,34 +96,18 @@ function Board({xIsNext, squares, onPlay}) {
       status = 'Next player: ' + (xIsNext ? 'X' : 'O');
     }
   }
-
-    // let squares = [];
-    // for (let j = 0; j < numRows; j++) { 
-    //   let squareRow = [];
-    //   for (let i = 0; i < numSquaresInRow; i++) {
-    //     squareRow.push(<Square key={numSquaresInRow*j+i} squareNumber={numSquaresInRow*j+i} value={squares[numSquaresInRow*j+i]} onSquareClick={onSquareClick}/>);
-    //   }
-    //   squares.push(<div className="board-row" key={j}>{squareRow}</div>);
-    // }
-    
+ 
   return (
     <>
       <div className="status"> {status} </div>
-      <div className="board-row">
-        <Square key={0} squareNumber={0} value={squares[0]} onSquareClick={() => onSquareClick(0)}/>
-        <Square key={1} squareNumber={1} value={squares[1]} onSquareClick={() => onSquareClick(1)}/>
-        <Square key={2} squareNumber={2} value={squares[2]} onSquareClick={() => onSquareClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square key={3} squareNumber={3} value={squares[3]} onSquareClick={() => onSquareClick(3)}/>
-        <Square key={4} squareNumber={4} value={squares[4]} onSquareClick={() => onSquareClick(4)}/>
-        <Square key={5} squareNumber={5} value={squares[5]} onSquareClick={() => onSquareClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square key={6} squareNumber={6} value={squares[6]} onSquareClick={() => onSquareClick(6)}/>
-        <Square key={7} squareNumber={7} value={squares[7]} onSquareClick={() => onSquareClick(7)}/>
-        <Square key={8} squareNumber={8} value={squares[8]} onSquareClick={() => onSquareClick(8)}/>
-      </div>
+      {[0,1,2].map((rowIndex) => (
+        <div className="board-row" key={rowIndex}>
+          {squares.slice(rowIndex*3, rowIndex*3+3).map((square, colIndex) => (
+            <Square key={rowIndex*3+colIndex} value={square} squareNumber={rowIndex*3+colIndex} 
+                  onSquareClick={() => onSquareClick(rowIndex*3+colIndex)}/>
+          ))}
+        </div>
+      ))}
     </>
   );
 }
